f4c8a42a8af78e13a07e9f41823e87302a5381a2
bumzack <bumkzacktraraqgmail.com>
2024-01-22 21:52:37 +0100

    ts impl

--- impls/tsc_node_koa/src/index.ts
+++ impls/tsc_node_koa/src/index.ts
@@ -6,11 +6,7 @@ import logger from 'koa-logger';
 
 import * as db from './db';
 import * as dotenv from "dotenv";
-import {getResolutions} from "./resolutionservice";
-import {Resolution} from "./models";
-import {processAllImagesV1} from "./image-utils-v1";
-import {processAllImagesV2} from "./image-utils-v2";
-import {getArticlesV3} from "./articleservice_v3";
+import {getArticles} from "./articleservice";
 
 const router = new Router();
 const app = new Koa();
@@ -30,10 +26,9 @@ router.get('/api/v1/articles/:pagenumber/:pagesize', async (ctx: Koa.Context, ne
     const pagesize = ctx.params.pagesize as number;
     console.log(`got url parameters?: pagenumber  ${pagenumber}  pagesize ${pagesize}  `);
     try {
-        const resolutions: Array<Resolution> = await getResolutions()
 
         const offset = pagenumber * pagesize
-        const articles = await processAllImagesV1(offset, pagesize, resolutions);
+        const articles = await getArticles(offset, pagesize);
         if (articles.length > 0) {
             ctx.body = JSON.stringify(articles)
         } else {
@@ -46,16 +41,10 @@ router.get('/api/v1/articles/:pagenumber/:pagesize', async (ctx: Koa.Context, ne
 });
 
 
-router.get('/api/v2/articles/:pagenumber/:pagesize', async (ctx: Koa.Context, next: Koa.Next) => {
+router.post('/api/v1/articles/import', async (ctx: Koa.Context, next: Koa.Next) => {
     const client = await db.pool.connect();
-    const pagenumber = ctx.params.pagenumber as number;
-    const pagesize = ctx.params.pagesize as number;
-    console.log(`got url parameters?: pagenumber  ${pagenumber}  pagesize ${pagesize}  `);
     try {
-        const resolutions: Array<Resolution> = await getResolutions()
-
-        const offset = pagenumber * pagesize
-        const articles = await processAllImagesV2(offset, pagesize, resolutions);
+        const articles = await importArticles();
         if (articles.length > 0) {
             ctx.body = JSON.stringify(articles)
         } else {
@@ -68,30 +57,8 @@ router.get('/api/v2/articles/:pagenumber/:pagesize', async (ctx: Koa.Context, ne
 });
 
 
-router.get('/api/v3/articles/:pagenumber/:pagesize', async (ctx: Koa.Context, next: Koa.Next) => {
-    const client = await db.pool.connect();
-    const pagenumber = ctx.params.pagenumber as number;
-    const pagesize = ctx.params.pagesize as number;
-    console.log(`got url parameters?: pagenumber  ${pagenumber}  pagesize ${pagesize}  `);
-    try {
-        // const resolutions: Array<Resolution> = await getResolutions()
 
-        const offset = pagenumber * pagesize
-        const articles = await getArticlesV3(offset, pagesize);
-        articles.forEach(e => {
-            e.image_as_json_pixels_array = ""
-            e.description = ""
-        })
-        if (articles.length > 0) {
-            ctx.body = JSON.stringify(articles)
-        } else {
-            ctx.status = 404
-        }
-    } finally {
-        client.release()
-    }
-    await next;
-});
+
 
 app.listen(port, () => {
     console.log(`started. listening on port ${port}`);
--- impls/tsc_node_koa/src/models/index.ts
+++ impls/tsc_node_koa/src/models/index.ts
@@ -1,71 +1,11 @@
-export interface ImageModel {
-    id: number;
-    filename: string;
-    image_as_rgb_png: string;
-    image_as_json_pixels_array: string;
-    width: number;
-    height: number;
-}
-
 export interface ArticleModel {
-    id: number;
     code: string;
     title: string;
     description: string;
-}
-
-export interface Art2ImgModel {
-    id: number;
-    article_id: number;
-    image_id: number;
-}
-
-
-export interface ResolutionModel {
-    id: number;
-    resolution: string;
-}
-
-
-export interface Image {
-    id: number;
-    filename: string,
-    image: string;
-    resolution: string;
-    width: number;
-    height: number;
-}
-
-export interface Pixel {
-    r: number;
-    g: number,
-    b: number;
-}
-
-export interface Resolution {
-    id: number;
-    name: string,
-    width: number;
-    height: number;
-    original: boolean;
-}
-
-
-export interface Article {
-    code: string;
-    title: string,
-    description: string;
-    images: Array<Image>;
-}
-
-
-export interface ArticleAndImage {
-    code: string;
-    title: string,
-    description: string;
-    id: number;
-    filename: string;
-    image_as_json_pixels_array: string;
-    width: number;
-    height: number;
+    attributes: string;
+    categories: string;
+    pos: string;
+    price: number;
+    start_date: Date;
+    end_date: Date;
 }
